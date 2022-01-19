5c2b9c34b20239ad7637b0692c0184b7b8cb35fc
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-19 22:13:03 +0900

    fasthttp server

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/lemon-mint/presigned
 
 go 1.17
+
+require (
+	github.com/lemon-mint/envaddr v0.0.0-20220115112619-b1acf77f317e
+	github.com/valyala/fasthttp v1.32.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.14.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/lemon-mint/envaddr"
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", envaddr.Get(":16702"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	server := fasthttp.Server{}
+	server.Name = "presigned"
+	server.ReadTimeout = time.Minute
+	server.WriteTimeout = time.Minute
+	server.IdleTimeout = time.Minute
+
+	SignalChan := make(chan os.Signal, 1)
+	signal.Notify(SignalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := server.Serve(ln); err != nil {
+			panic(err)
+		}
+		os.Exit(0)
+	}()
+
+	<-SignalChan
+	log.Println("Shutting down the server...")
+	err = server.Shutdown()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Server gracefully stopped")
+}
